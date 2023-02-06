286fb3ea0115a1ec26410bcdf4261d8bb40bf1a7
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-06 10:07:09 +0530

    overriden default security filter chain for basic auth

---
+++ boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.sparkdev.securityworkshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
+
+    }
+}
