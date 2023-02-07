ae62f0ddc11d7fa50a50c45b9e8c031151019932
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-07 19:45:25 +0530

    modified the security config with request matchers to have auth specific to some apis

--- boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/config/SecurityConfig.java
+++ boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/config/SecurityConfig.java
@@ -10,10 +10,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    // open the /greet api without security
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+        return httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
-                .anyRequest()
+                .requestMatchers("/api/v1/greet").permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/**")
                 .authenticated()
                 .and()
                 .httpBasic()
