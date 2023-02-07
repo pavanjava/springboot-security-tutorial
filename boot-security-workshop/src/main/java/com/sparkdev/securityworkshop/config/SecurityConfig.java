af982a90ac3bc5832c101ec26f5d5b1a01467722
Pavan Mantha <manthapavankumar11@gmail.com>
2023-02-07 23:36:34 +0530

    modified the security config to add authentication and authorization basic level along with method level security

--- boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/config/SecurityConfig.java
+++ boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/config/SecurityConfig.java
@@ -1,16 +1,39 @@
 package com.sparkdev.securityworkshop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Value("${application.user1.name}")
+    private String adminUserName;
+    @Value("${application.user1.password}")
+    private String adminUserPassword;
+    @Value("${application.user1.role}")
+    private String adminRole;
+
+    @Value("${application.user2.name}")
+    private String normalUserName;
+    @Value("${application.user2.password}")
+    private String normalUserPassword;
+    @Value("${application.user2.role}")
+    private String userRole;
     @Bean
-    // open the /greet api without security
+    // open the /greet api without security.
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
@@ -24,5 +47,26 @@ public class SecurityConfig {
                 .and()
                 .build();
 
+    }
+
+    // below method serve the purpose of Authentication and Authorization
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails adminUser = User.withUsername(adminUserName)
+                .password(passwordEncoder.encode(adminUserPassword))
+                .roles(adminRole)
+                .build();
+
+        UserDetails normalUser = User.withUsername(normalUserName)
+                .password(passwordEncoder.encode(normalUserPassword))
+                .roles(userRole)
+                .build();
+
+        return new InMemoryUserDetailsManager(adminUser, normalUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/controller/GreetingController.java
+++ boot-security-workshop/src/main/java/com/sparkdev/securityworkshop/controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.sparkdev.securityworkshop.controller;
 import com.sparkdev.securityworkshop.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class GreetingController {
     }
 
     @GetMapping("/goodbye")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<String> sayGoodbye(){
         return ResponseEntity.ok(greetingService.goodBye());
     }
